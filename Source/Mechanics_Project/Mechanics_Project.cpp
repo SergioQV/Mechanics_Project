34b487fae8356b35da6012fb09d4cf46bca3a61b
SergioQV <sergiolordi@hotmail.es>
2023-12-14 02:13:41 +0100

    first commit

---
+++ Source/Mechanics_Project/Mechanics_Project.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Mechanics_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mechanics_Project, "Mechanics_Project" );
+ 
